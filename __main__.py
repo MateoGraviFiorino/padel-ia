0fd58879cf3f509f1f2c05a4baefe130fc494020
tomas <tomas.navarro200013@gmail.com>
2025-08-21 17:14:31 -0300

    fixing bugs

--- __main__.py
+++ __main__.py
@@ -27,7 +27,7 @@ app.include_router(status_router)
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "__main__:app",
         host="0.0.0.0",
         port=8000,
         reload=True,
